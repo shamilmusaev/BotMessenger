16abb41bf9b5069213b9bbcdd4c07769566aa135
Dwhitewolf1995 <shamil.musaev@iths.se>
2023-08-16 00:37:21 +0200

    more styles and animations were added

--- promptArt.js
+++ promptArt.js
@@ -1,103 +1,109 @@
 async function query(data) {
-    const response = await fetch(
-      "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
-      {
-        headers: { Authorization: "Bearer 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" },
-        method: "POST",
-        body: JSON.stringify(data),
-      }
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
+    {
+      headers: {
+        Authorization: "Bearer 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",
+      },
+      method: "POST",
+      body: JSON.stringify(data),
+    }
+  );
+  const result = await response.blob();
+  return result;
+}
+
+async function animateMessage(messageText) {
+  const messageElement = document.createElement("div");
+  messageElement.classList.add("message", "user-message");
+  messagesContainer.appendChild(messageElement);
+
+  const delay = 70;
+
+  for (let i = 0; i <= messageText.length; i++) {
+    messageElement.textContent = messageText.substring(0, i);
+    await new Promise((resolve) => setTimeout(resolve, delay));
+  }
+}
+
+const generateButton = document.getElementById("send-button");
+const messageInput = document.getElementById("message-input");
+const messagesContainer = document.querySelector(".messages");
+
+generateButton.addEventListener("click", async () => {
+  const messageText = messageInput.value;
+  if (messageText.trim() !== "") {
+    const userMessageElement = document.createElement("div");
+    userMessageElement.classList.add("message", "user-message");
+    userMessageElement.textContent = messageText;
+    messagesContainer.appendChild(userMessageElement);
+    messageInput.value = "";
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+
+    const loadingIndicator = document.createElement("div");
+    loadingIndicator.classList.add(
+      "message",
+      "other-message",
+      "typing-indicator"
     );
-    const result = await response.blob();
-    return result;
+    const dots = document.createElement("span");
+    dots.classList.add("dot");
+    loadingIndicator.appendChild(dots);
+    loadingIndicator.appendChild(dots.cloneNode());
+    loadingIndicator.appendChild(dots.cloneNode());
+    messagesContainer.appendChild(loadingIndicator);
+
+    const imageData = await query({ inputs: messageText });
+    const imageUrl = URL.createObjectURL(imageData);
+
+    messagesContainer.removeChild(loadingIndicator);
+
+    const imageElement = document.createElement("img");
+    imageElement.classList.add("img_container");
+    imageElement.src = imageUrl;
+    imageElement.alt = "Not available";
+    messagesContainer.appendChild(imageElement);
+
+    // Прокрутить контейнер к последнему элементу
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
   }
-  
-  async function animateMessage(messageText) {
-    const messageElement = document.createElement("div");
-    messageElement.classList.add("message", "user-message");
-    messagesContainer.appendChild(messageElement);
-  
-    const delay = 70;
-  
-    for (let i = 0; i <= messageText.length; i++) {
-      messageElement.textContent = messageText.substring(0, i);
-      await new Promise(resolve => setTimeout(resolve, delay));
-    }
-  }
-  
-  const generateButton = document.getElementById("send-button");
-  const messageInput = document.getElementById("message-input");
+});
+
+document.addEventListener("DOMContentLoaded", async function () {
   const messagesContainer = document.querySelector(".messages");
-  
-  generateButton.addEventListener("click", async () => {
-    const messageText = messageInput.value;
-    if (messageText.trim() !== "") {
-      const userMessageElement = document.createElement("div");
-      userMessageElement.classList.add("message", "user-message");
-      userMessageElement.textContent = messageText;
-      messagesContainer.appendChild(userMessageElement);
-      messageInput.value = "";
-      messagesContainer.scrollTop = messagesContainer.scrollHeight;
-  
-      const loadingIndicator = document.createElement("div");
-      loadingIndicator.classList.add("message", "other-message", "typing-indicator");
-      const dots = document.createElement("span");
-      dots.classList.add("dot");
-      loadingIndicator.appendChild(dots);
-      loadingIndicator.appendChild(dots.cloneNode());
-      loadingIndicator.appendChild(dots.cloneNode());
-      messagesContainer.appendChild(loadingIndicator);
-  
-      const imageData = await query({ inputs: messageText });
-      const imageUrl = URL.createObjectURL(imageData);
-  
-      messagesContainer.removeChild(loadingIndicator);
-  
-      const imageElement = document.createElement("img");
-      imageElement.classList.add("img_container");
-      imageElement.src = imageUrl;
-      imageElement.alt = "Not available";
-      messagesContainer.appendChild(imageElement);
-  
-  
-      // Прокрутить контейнер к последнему элементу
-      messagesContainer.scrollTop = messagesContainer.scrollHeight;
-      
+  const delay = 10;
+
+  const messages = [
+    "Hello. Type a prompt and get awesome images 😜",
+    "For example ",
+    "Surreal illustration of Stockholm camera zoom 0.5 🇸🇪",
+    "Space scene in Van Gogh style 🖼️",
+    "Cityscape at night in cyberpunk style 🏙️",
+    "Make sure to add to your prompt as much details as possible",
+    "Have fun 😊",
+  ];
+
+  async function animateMessage(message) {
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("other-message");
+    messagesContainer.appendChild(messageElement);
+
+    for (let i = 0; i <= message.length; i++) {
+      messageElement.textContent = message.substring(0, i);
+      await new Promise((resolve) => setTimeout(resolve, delay));
     }
-  });
-  
-  
-  document.addEventListener("DOMContentLoaded", async function () {
-    const messagesContainer = document.querySelector(".messages");
-    const delay = 10;
-  
-    const messages = [
-      "Hello. Type a prompt and get awesome images 😜",
-      "For example ",
-      "Surreal illustration of Stockholm camera zoom 0.5 🇸🇪",
-      "Space scene in Van Gogh style 🖼️",
-      "Cityscape at night in cyberpunk style 🏙️",
-      "Make sure to add to your prompt as much details as possible", 
-      "Have fun 😊"
-    ];
-  
-    async function animateMessage(message) {
-      const messageElement = document.createElement("div");
-      messageElement.classList.add("other-message");
-      messagesContainer.appendChild(messageElement);
-  
-      for (let i = 0; i <= message.length; i++) {
-        messageElement.textContent = message.substring(0, i);
-        await new Promise(resolve => setTimeout(resolve, delay));
-      }
-  
-      await new Promise(resolve => setTimeout(resolve, 1000)); 
-    }
-  
-    for (const message of messages) {
-      await animateMessage(message);
-    }
-  });
-  
-  
-  
-  
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+
+  for (const message of messages) {
+    await animateMessage(message);
+  }
+});
+
+const switcher = document.querySelector(".theme");
+
+switcher.addEventListener("click", () => {
+  document.querySelector(".imessage-container").classList.toggle("dark-theme");
+  document.querySelector("body").classList.toggle("dark-theme");
+});
--- sarah.js
+++ sarah.js
@@ -1,127 +1,125 @@
 async function query(data) {
-    const randomParam = Math.random(); 
-    const modifiedData = { ...data, randomParam };
-  
-    const response = await fetch(
-      "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits",
-      {
-        headers: {
-          Authorization: "Bearer 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",
-        },
-        method: "POST",
-        body: JSON.stringify(modifiedData),
-      }
+  const randomParam = Math.random();
+  const modifiedData = { ...data, randomParam };
+
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits",
+    {
+      headers: {
+        Authorization: "Bearer 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",
+      },
+      method: "POST",
+      body: JSON.stringify(modifiedData),
+    }
+  );
+  const result = await response.blob();
+  return result;
+}
+
+async function chat(data) {
+  const randomParam = Math.random();
+  const modifiedData = { ...data, randomParam };
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill",
+    {
+      headers: {
+        Authorization: "Bearer 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",
+      },
+      method: "POST",
+      body: JSON.stringify(modifiedData),
+    }
+  );
+  const result = await response.json();
+  return result;
+}
+
+const generateButton = document.getElementById("send-button");
+const messageInput = document.getElementById("message-input");
+const messagesContainer = document.querySelector(".messages");
+
+generateButton.addEventListener("click", async () => {
+  const messageText = messageInput.value;
+  if (messageText.trim() !== "") {
+    const userMessageElement = document.createElement("div");
+    userMessageElement.classList.add("message", "user-message");
+    userMessageElement.textContent = messageText;
+    messagesContainer.appendChild(userMessageElement);
+    messageInput.value = "";
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+
+    const loadingIndicator = document.createElement("div");
+    loadingIndicator.classList.add(
+      "message",
+      "other-message",
+      "typing-indicator"
     );
-    const result = await response.blob();
-    return result;
+    const dots = document.createElement("span");
+    dots.classList.add("dot");
+    loadingIndicator.appendChild(dots);
+    loadingIndicator.appendChild(dots.cloneNode());
+    loadingIndicator.appendChild(dots.cloneNode());
+    messagesContainer.appendChild(loadingIndicator);
+
+    const chatbotResponse = await chat({ inputs: messageText });
+
+    //For text
+    // const botMessageElement = document.createElement("div")
+    // botMessageElement.classList.add("message", "bot-message");
+    // botMessageElement.textContent = chatbotResponse.generated_text;
+
+    // messagesContainer.appendChild(botMessageElement)
+
+    const audioData = await query({ inputs: chatbotResponse.generated_text });
+    const audioUrl = URL.createObjectURL(audioData);
+
+    messagesContainer.removeChild(loadingIndicator);
+
+    const audioContainer = document.createElement("div");
+    audioContainer.classList.add("audio-container");
+
+    const audioElement = document.createElement("audio");
+    audioElement.controls = true;
+    audioElement.src = audioUrl;
+
+    audioContainer.appendChild(audioElement);
+    messagesContainer.appendChild(audioContainer);
+
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
   }
-  
-  async function chat(data) {
-    const randomParam = Math.random(); 
-    const modifiedData = { ...data, randomParam };
-    const response = await fetch(
-      "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill",
-      {
-        headers: {
-          Authorization: "Bearer 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",
-        },
-        method: "POST",
-        body: JSON.stringify(modifiedData),
-      }
-    );
-    const result = await response.json();
-    return result;
-  }
-  
-  // async function animateMessage(messageText) {
-  //   const messageElement = document.createElement("div");
-  //   messageElement.classList.add("message", "user-message");
-  //   messagesContainer.appendChild(messageElement);
-  
-  //   const delay = 70;
-  
-  //   for (let i = 0; i <= messageText.length; i++) {
-  //     messageElement.textContent = messageText.substring(0, i);
-  //     await new Promise(resolve => setTimeout(resolve, delay));
-  //   }
-  // }
-  
-  const generateButton = document.getElementById("send-button");
-  const messageInput = document.getElementById("message-input");
+});
+
+document.addEventListener("DOMContentLoaded", async function () {
   const messagesContainer = document.querySelector(".messages");
-  
-  generateButton.addEventListener("click", async () => {
-    const messageText = messageInput.value;
-    if (messageText.trim() !== "") {
-      const userMessageElement = document.createElement("div");
-      userMessageElement.classList.add("message", "user-message");
-      userMessageElement.textContent = messageText;
-      messagesContainer.appendChild(userMessageElement);
-      messageInput.value = "";
-      messagesContainer.scrollTop = messagesContainer.scrollHeight;
-  
-      const loadingIndicator = document.createElement("div");
-      loadingIndicator.classList.add(
-        "message",
-        "other-message",
-        "typing-indicator"
-      );
-      const dots = document.createElement("span");
-      dots.classList.add("dot");
-      loadingIndicator.appendChild(dots);
-      loadingIndicator.appendChild(dots.cloneNode());
-      loadingIndicator.appendChild(dots.cloneNode());
-      messagesContainer.appendChild(loadingIndicator);
-  
-      const chatbotResponse = await chat({ inputs: messageText });
-      // const botMessageElement = document.createElement("div")
-      // botMessageElement.classList.add("message", "bot-message");
-      // botMessageElement.textContent = chatbotResponse.generated_text;
-  
-      // messagesContainer.appendChild(botMessageElement)
-  
-      const audioData = await query({ inputs: chatbotResponse.generated_text });
-      const audioUrl = URL.createObjectURL(audioData);
-  
-      messagesContainer.removeChild(loadingIndicator);
-  
-      const audioElement = document.createElement("audio");
-      audioElement.controls = true; 
-      audioElement.src = audioUrl;
-      messagesContainer.appendChild(audioElement);
-  
-      
-      messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  const delay = 10;
+
+  const messages = [
+    "Hi.",
+    "My name is Sarah 😜",
+    "Text me, and I will answer you with a voice message 👻",
+    "Let's talk! 🥰",
+  ];
+
+  async function animateMessage(message) {
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("other-message");
+    messagesContainer.appendChild(messageElement);
+
+    for (let i = 0; i <= message.length; i++) {
+      messageElement.textContent = message.substring(0, i);
+      await new Promise((resolve) => setTimeout(resolve, delay));
     }
-  });
-  
-  document.addEventListener("DOMContentLoaded", async function () {
-    const messagesContainer = document.querySelector(".messages");
-    const delay = 10;
-  
-    const messages = [
-      "Hi.",
-      "My name is Sarah 😜",
-      "Text me, and I will answer you with a voice message 👻",
-      "Let's talk! 🥰"
-  
-    ];
-  
-    async function animateMessage(message) {
-      const messageElement = document.createElement("div");
-      messageElement.classList.add("other-message");
-      messagesContainer.appendChild(messageElement);
-  
-      for (let i = 0; i <= message.length; i++) {
-        messageElement.textContent = message.substring(0, i);
-        await new Promise(resolve => setTimeout(resolve, delay));
-      }
-  
-      await new Promise(resolve => setTimeout(resolve, 1000)); 
-    }
-  
-    for (const message of messages) {
-      await animateMessage(message);
-    }
-  });
-  
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+
+  for (const message of messages) {
+    await animateMessage(message);
+  }
+});
+
+const switcher = document.querySelector(".theme");
+
+switcher.addEventListener("click", () => {
+  document.querySelector(".imessage-container").classList.toggle("dark-theme");
+  document.querySelector("body").classList.toggle("dark-theme");
+});
